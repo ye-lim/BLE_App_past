139c4e0265e822d475b964d015bf979002517887
yelim <720angela98@gmail.com>
2020-05-26 11:56:30 +0900

    Designed dialog for amount_change_fragment

--- app/src/main/java/com/example/blu_main_test1/amount_change_fragment.java
+++ app/src/main/java/com/example/blu_main_test1/amount_change_fragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
